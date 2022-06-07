c70d99f5253b742d00ece020ff8a2872ac0eb86f
Jonas <jonasdhein@gmail.com>
2022-06-07 17:31:03 -0300

    Primeiro Commit

---
+++ src/aula1/Aluno.java
@@ -0,0 +1,18 @@
+package aula1;
+
+/**
+ *
+ * @author jonasdhein
+ */
+public class Aluno {
+    
+    public String nome;
+    public int idade;
+    
+    public void fazAniversario(){
+        idade = idade + 1;
+    }
+    
+    
+    
+}
---
+++ src/aula1/ExerciciosAluno.java
@@ -0,0 +1,37 @@
+package aula1;
+/**
+ *
+ * @author jonasdhein
+ */
+public class ExerciciosAluno {
+    
+    //public => É o modificador de acesso
+    //permite que o método fique visível para outras classes
+    //private => Permite que apenas a própria classe utilize o método
+    //protected => ?
+    //static => permite que o método seja invocado sem necessidade de criar uma
+    //nova instância
+    //void => o método em questão não tem retorno
+    public static void main(String[] args){
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.idade = 36;
+        aluno1.nome = "Bruno";
+        
+        Aluno aluno2 = new Aluno();
+        aluno2.idade = 23;
+        aluno2.nome = "Yuri";
+        
+        
+        //o método imprimir deve mostrar o nome e a idade do aluno juntos
+        //System.out.println("Aluno1 = " + aluno1.imprimir());
+        //System.out.println("Aluno2 = " + aluno2.imprimir());
+        
+        aluno2.fazAniversario();
+        //System.out.println("Aluno2 = " + aluno2.imprimir());
+        
+        
+    }
+    
+    
+}
